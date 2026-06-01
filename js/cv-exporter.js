642d8f56ced77bd7c70e4f27cdb125562f69842b
MarlonJunio-NSJBI <marlon.junio@nsjbi.com.ph>
2026-06-01 18:12:37 +0800

    Add Download CV

---
+++ js/cv-exporter.js
@@ -0,0 +1,15 @@
+﻿window.cvExporter = {
+    generatePDF: function () {
+        const element = document.querySelector('.cv-paper');
+
+        const opt = {
+            margin: 0,
+            filename: 'Marlon_Junio_Resume.pdf',
+            image: { type: 'jpeg', quality: 0.98 },
+            html2canvas: { scale: 2, useCORS: true },
+            jsPDF: { unit: 'mm', format: 'a4', orientation: 'portrait' }
+        };
+
+        html2pdf().set(opt).from(element).save();
+    }
+};
